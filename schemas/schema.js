9c142664ca38b4edffa5c911aef32ed3c1a8fbd9
Rikard Eide <rikardeide@gmail.com>
2019-11-14 21:51:33 +0100

    eslint: setup eslint and format files

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,14 +1,16 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
+// eslint-disable-next-line import/no-unresolved
+import createSchema from 'part:@sanity/base/schema-creator';
 
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+// eslint-disable-next-line import/no-unresolved
+import schemaTypes from 'all:part:@sanity/base/schema-type';
 
 // We import object and document schemas
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
+import blockContent from './blockContent';
+import category from './category';
+import post from './post';
+import author from './author';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -24,6 +26,6 @@ export default createSchema({
     category,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
-    blockContent
-  ])
-})
+    blockContent,
+  ]),
+});
