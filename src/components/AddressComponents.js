0f9f0e70653576b94b5b9330ae6ca4d7c0c617f5
Rikard Eide <rikardeide@gmail.com>
2019-11-24 22:02:31 +0100

    ContactPage: add content to /kontakt page

---
+++ src/components/AddressComponents.js
@@ -0,0 +1,40 @@
+function formatNumber(number, type) {
+  if (type === "cell") {
+    return [number.slice(0, 3), number.slice(3, 5), number.slice(5)].join(" ");
+  }
+  return [
+    number.slice(0, 2),
+    number.slice(2, 4),
+    number.slice(4, 6),
+    number.slice(6)
+  ].join(" ");
+}
+
+export const AddressLink = ({ children, ...rest }) => (
+  <a className="f5 link dim black-70" {...rest}>
+    {children}
+  </a>
+);
+
+export const Phone = ({ children, type, ...rest }) => {
+  if (typeof children !== "string") {
+    throw new Error(`Only strings allowed as children. Got ${typeof children}`);
+  }
+  return (
+    <AddressLink href={`tel:${children}`} {...rest}>
+      {formatNumber(children, type)}
+    </AddressLink>
+  );
+};
+
+export const Office = ({ children, ...rest }) => {
+  return (
+    <article className="flex-auto w-50">
+      <address className="fs-normal">{children}</address>
+    </article>
+  );
+};
+
+export const AddressLine = ({ children }) => {
+  return <span className="f5 db black-70 lh-copy">{children}</span>;
+};
--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -1,7 +1,61 @@
 import Page from "../components/Page";
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
+import { PageTitle, PageContent, Text } from "../components/PageComponents";
+import {
+  AddressLink,
+  Phone,
+  Office,
+  AddressLine
+} from "../components/AddressComponents";
 
-const Contact = () => {
-  return <Page title="Kontaktinfo"></Page>;
+const ContactPage = () => {
+  return (
+    <Page title="Kontaktinfo">
+      <Navigation color="black" />
+      <PageContent>
+        <PageTitle color="black">Kontaktinfo</PageTitle>
+        <Text>
+          Vi er stort sett på feriekoloien hele sommeren, resten av året finner
+          du oss i Oslo.
+        </Text>
+
+        <div className="flex mb5">
+          <Office>
+            <b className="f5 f4-l fw6 dib mb3 mt4">Filtvet Feriekoloni</b>
+            <AddressLine>Trefoldighet Feriekoloni Stranda 16</AddressLine>
+            <AddressLine>Postboks 3, 3480 Filtvet</AddressLine>
+
+            <div className="f5 fw6 pv3 black-70">
+              Tlf:{" "}
+              <Phone title="Ring feriekoloniens fasttelefon">32796270</Phone>{" "}
+              <span className="fw4">|</span>{" "}
+              <Phone title="Ring feriekoloniens andre fasttelefon">
+                32796270
+              </Phone>
+            </div>
+            <AddressLink href="mailTo:feriekolonien@feriekolonien.no">
+              feriekolonien@feriekolonien.no
+            </AddressLink>
+          </Office>
+
+          <Office>
+            <b className="f5 f4-l fw6 dib mb3 mt4">Oslo</b>
+            <AddressLine>Trefoldighet Feriekoloni Postboks 6929</AddressLine>
+            <AddressLine>St. Olavs Plass, 0130 Oslo</AddressLine>
+
+            <div className="f5 fw6 pv3 black-70">
+              Elins tlf: <Phone title="Ring Elin">48111106</Phone>
+            </div>
+            <AddressLink href="mailTo:feriekolonien@feriekolonien.no">
+              feriekolonien@feriekolonien.no
+            </AddressLink>
+          </Office>
+        </div>
+      </PageContent>
+      <Footer />
+    </Page>
+  );
 };
 
-export default Contact;
+export default ContactPage;
