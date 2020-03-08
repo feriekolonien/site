1367796ef9423b6629cb8b34727cff9083a561b2
Rikard Eide <rikardeide@gmail.com>
2020-03-08 17:17:00 +0100

    script: finish upload script

--- scripts/uploadPhotos.js
+++ scripts/uploadPhotos.js
@@ -1,9 +1,15 @@
 import { basename, join } from 'path';
-import { createReadStream, readFileSync, writeFile } from 'fs';
-import fg from 'fast-glob';
+import { createReadStream } from 'fs';
 
+// eslint-disable-next-line import/no-extraneous-dependencies
+import fg from 'fast-glob';
 import sanityClient from '@sanity/client'; // eslint-disable-line
 import nanoid from 'nanoid'; // eslint-disable-line
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 const token =
   'skrAFHdJFvrhSaubpLTp3SfzSt1rhG3wLeTGzTmQ1LWRiXOvJiU4JUfZ4qZLXXHwZ0P4hy007WvsPctTsDent5E54vMfj0izCIizVg6Fa4HdNaFcrpBEXVha4Y7hIrrwkEVDVcnuIzqh7lrvsqBWmMfUueDhM2cXoPmwWz4QoyWGunMKW58w';
@@ -64,18 +70,47 @@ async function runScript() {
   );
   const startPath = getAbsolutePath();
   console.log(join(startPath, '**/*.jpg'));
-  const entries = await fg([join(startPath, '**/*.jpg')]);
+  const entries = await fg([join(startPath, '**/*.jpg')], {
+    caseSensitiveMatch: false,
+  });
   const paginated = createChunks(entries, 3);
+  readline.question(
+    `Will upload ${entries.length} files in ${paginated.length} batches. Continue? (y/N) `,
+    async answer => {
+      if (answer && answer.toLowerCase() === 'y') {
+        const uploadedImages = [];
+        for (let index = 0; index < paginated.length; index++) {
+          console.log(
+            `starting batch #${index} (${paginated[index].length} files)`,
+          );
+          const waitForFileUpload = paginated[index].map(file =>
+            uploadImageAsset(file),
+          ); // returns promise
 
-  console.log(
-    `Will upload ${entries.length} files in ${paginated.length} batches`,
+          // eslint-disable-next-line no-await-in-loop
+          const images = await Promise.all(waitForFileUpload);
+          uploadedImages.push(images);
+
+          // eslint-disable-next-line no-await-in-loop
+        }
+        const flattened = uploadedImages.flat();
+        const [, id] = process.argv.slice(2);
+        if (id) {
+          console.log(`Adding images to album ${id}`);
+          const addToAlbums = flattened.map(image =>
+            addImageToAlbum(id, image),
+          );
+          await Promise.all(addToAlbums);
+        } else {
+          console.log('Ok, will not add to album. Exiting.');
+        }
+      } else {
+        console.log('Ok, will not do anything. Exiting.');
+      }
+      readline.close();
+      process.exit(0);
+    },
   );
-  for (let index = 0; index < paginated.length; index++) {
-    console.log(`starting batch #${index} (${paginated[index].length} files)`);
-    const promises = paginated[index].map(file => uploadImageAsset(file)); // returns promise
-    await Promise.all(promises); // eslint-disable-line
-  }
-  console.log('...done');
 
   // console.log('/scripts/2013/2013_001.jpg');
   // console.log(client);
