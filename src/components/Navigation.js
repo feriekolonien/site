40a74b87063acd5e6e84974c09643af3cd2a53a9
Siri Holtnæs <siri@nettbil.no>
2020-03-13 18:03:29 +0100

    Make nav visible on mobile

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,7 @@ const NavLink = ({ children, href, color }) => {
   return (
     <Link href={href}>
       <a
-        className={`f5 fw5 hover-${color} no-underline ${color} dn dib-ns pv2 ph3`}>
+        className={`f5 fw5 hover-${color} no-underline ${color} dib-ns pv2 ph3`}>
         {children}
       </a>
     </Link>
@@ -13,15 +13,19 @@ const NavLink = ({ children, href, color }) => {
 
 const Navigation = ({ color = 'white' }) => {
   return (
-    <nav className="flex w-100 mw8 center">
-      <div className="pa3 mr-auto">
+    <nav className="flex w-100 mw8 center flex-row-ns flex-column">
+      <div className="pa3 mr-auto-ns">
         <Link href="/">
           <a className="dib grow-large border-box">
-            <img src="/logo.png" className="w5" alt="Filtvet feriekoloni" />
+            <img
+              src="/logo.png"
+              className="w-50 w5-ns"
+              alt="Filtvet feriekoloni"
+            />
           </a>
         </Link>
       </div>
-      <div className="tr pa3">
+      <div className="tr-ns pa3">
         <NavLink color={color} href="/om-oss">
           Om oss
         </NavLink>
