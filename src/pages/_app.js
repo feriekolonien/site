16e098dcd3114117cfb0a3b3bd81c154bb9141b6
Rikard Eide <rikardeide@gmail.com>
2021-10-31 20:26:20 +0100

    ts: rename all files to .ts and .tsx – here goes nothing

--- src/pages/_app.js
+++
@@ -1,7 +0,0 @@
-import '../styles/styles.css';
-
-// eslint-disable-next-line react/prop-types
-export default function MyApp({ Component, pageProps }) {
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />;
-}
