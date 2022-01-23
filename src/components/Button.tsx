b83e971d1ce77a5976ca90d90c5f7cf7318d0f92
Rikard Eide <rikardeide@gmail.com>
2022-01-23 18:59:41 +0100

    Button: Add hover states and transition

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,14 @@ type ButtonProps = {
 
 const Button = ({ children }: ButtonProps) => {
   return (
-    <button className="font-bold bold text-xl px-8 py-2 rounded-full text-[#332A00] bg-gradient-to-tr from-[#FC9C0C] to-[#FFD930]">
+    <button
+      className={[
+        '',
+        'transition-all duration-150',
+        'font-bold bold text-xl px-8 py-2 rounded-full text-[#332A00] bg-gradient-to-tr from-[#FC9C0C] to-[#FFD930]',
+        'hover:bg-yellow-300 hover:bg-blend-multiply active:bg-blend-multiply active:bg-yellow-400',
+      ].join(' ')}
+    >
       {children}
     </button>
   );
