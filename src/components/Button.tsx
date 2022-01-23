45e6641d14dce449c9193419832701380c207581
Rikard Eide <rikardeide@gmail.com>
2022-01-23 22:49:26 +0100

    Button: Add another CTA component ButtonLink for rendering anchor tags

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,9 @@
+import Link from 'next/link';
+
 type ButtonProps = {
   children: React.ReactNode;
   className?: string;
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
 const Button = ({ children, className }: ButtonProps) => {
@@ -16,6 +19,30 @@ const Button = ({ children, className }: ButtonProps) => {
     >
       {children}
     </button>
+  );
+};
+
+type ButtonLinkProps = {
+  href: string;
+  children: React.ReactNode;
+  className?: string;
+};
+
+export const ButtonLink = ({ children, className, href }: ButtonLinkProps) => {
+  return (
+    <Link href={href}>
+      <a
+        className={[
+          className,
+          'font-bold',
+          'transition-all duration-150',
+          'px-8 py-2 rounded-full text-[#332A00] bg-gradient-to-tr from-[#FC9C0C] to-[#FFD930]',
+          'hover:bg-yellow-300 hover:bg-blend-multiply active:bg-blend-multiply active:bg-yellow-400',
+        ].join(' ')}
+      >
+        {children}
+      </a>
+    </Link>
   );
 };
 
