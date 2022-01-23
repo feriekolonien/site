7c7e4a887cdaac290bc3737ea14e57e13ce7f05d
Siri Holtnæs <s.holtnaes@gmail.com>
2022-01-23 22:35:02 +0100

    feat: add crabs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,12 @@ import SandSVG from './SandSVG';
 
 const Footer = ({ className = '', showSand = false }) => (
   <footer className={className}>
+    {showSand && (
+      <img
+        src="/static/img/crab.png"
+        className="absolute right-10 bottom-10 md:right-40 md:bottom-20 z-20"
+      />
+    )}
     {showSand && <SandSVG />}
     <div className="text-center pb-3 text-yellow-700">
       <small className="relative z-10">
