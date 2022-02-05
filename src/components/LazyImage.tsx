467d2936149a217cafe8e5669bd0a381dbffc048
Rikard Eide <rikardeide@gmail.com>
2022-02-05 09:22:04 +0100

    LazyImage: Make alt prop required

--- src/components/LazyImage.tsx
+++ src/components/LazyImage.tsx
@@ -2,18 +2,24 @@ import * as React from 'react';
 // import useNativeLazyLoading from '@charlietango/use-native-lazy-loading';
 import { useInView } from 'react-intersection-observer';
 
-const LazyImage = ({
-  src,
-  crossOrigin,
-  ...rest
-}: React.HTMLProps<HTMLImageElement>) => {
+type LazyImageProps = {
+  src: string;
+  alt: string;
+} & React.HTMLProps<HTMLImageElement>;
+const LazyImage = ({ src, alt, crossOrigin, ...rest }: LazyImageProps) => {
   const { ref, inView } = useInView({
     triggerOnce: true,
     rootMargin: '200px 0px',
   });
 
   return (
-    <img {...rest} ref={ref} src={inView ? src : undefined} loading="lazy" />
+    <img
+      {...rest}
+      alt={alt}
+      ref={ref}
+      src={inView ? src : undefined}
+      loading="lazy"
+    />
   );
 };
 
