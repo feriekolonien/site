12f2ec9b5b9ad01ef8bd8ee3ea7e4e68440a91ff
Rikard Eide <rikardeide@gmail.com>
2022-02-05 09:09:46 +0100

    LazyImage: Convert component to TypeScript

--- src/components/LazyImage.tsx
+++ src/components/LazyImage.tsx
@@ -1,22 +1,19 @@
-import React from 'react';
+import * as React from 'react';
 // import useNativeLazyLoading from '@charlietango/use-native-lazy-loading';
 import { useInView } from 'react-intersection-observer';
 
-const LazyImage = ({ width, height, src, ...rest }) => {
+const LazyImage = ({
+  src,
+  crossOrigin,
+  ...rest
+}: React.HTMLProps<HTMLImageElement>) => {
   const { ref, inView } = useInView({
     triggerOnce: true,
     rootMargin: '200px 0px',
   });
 
   return (
-    <img
-      {...rest}
-      ref={ref}
-      src={inView ? src : undefined}
-      width={width}
-      height={height}
-      loading="lazy"
-    />
+    <img {...rest} ref={ref} src={inView ? src : undefined} loading="lazy" />
   );
 };
 
