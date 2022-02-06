6569a447c4233b976e86b8ce25cbd039ed201e01
Rikard Eide <rikardeide@gmail.com>
2022-02-06 12:35:33 +0100

    cms: update configuration files (prettier & eslint)

--- packages/config/eslint-preset.js
+++ packages/config/eslint-preset.js
@@ -10,6 +10,18 @@ module.exports = {
   },
 };
 
+/**
+ *     "@otovo/eslint-config-otovo": "^3.0.0",
+    "babel-eslint": "^10.0.3",
+    "eslint": "^6.6.0",
+    "eslint-config-airbnb": "^18.0.1",
+    "eslint-config-prettier": "^6.5.0",
+    "eslint-plugin-import": "^2.18.2",
+    "eslint-plugin-jsx-a11y": "^6.2.3",
+    "eslint-plugin-prettier": "^3.1.1",
+    "eslint-plugin-react": "^7.16.0",
+ */
+
 // Previous configuration that came with Turborepo
 // module.exports = {
 //   extends: ["next", "prettier"],
