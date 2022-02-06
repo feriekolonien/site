bd622608cf0c4b21d75d154463cc750fc52f011a
Rikard Eide <rikardeide@gmail.com>
2022-02-06 23:20:53 +0100

    web: add module boundary type sanity lib wrapper

--- apps/web/src/lib/sanity.server.ts
+++ apps/web/src/lib/sanity.server.ts
@@ -1,5 +1,6 @@
+import { SanityClient } from '@sanity/client';
 import { createClient } from 'next-sanity';
 import { config } from './config';
 
 // Set up the client for fetching data in the getProps page functions
-export const sanityClient = createClient(config);
+export const sanityClient: SanityClient = createClient(config);
