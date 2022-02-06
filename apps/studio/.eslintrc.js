cbcebfe39d0fd37e00bcf9c74a15c0e3e4e8358a
Rikard Eide <rikardeide@gmail.com>
2022-02-06 17:38:15 +0100

    studio: use Sanity's ESlint config

--- apps/studio/.eslintrc.js
+++ apps/studio/.eslintrc.js
@@ -1,1 +1,7 @@
-module.exports = require('config/eslint-preset');
+module.exports = {
+  extends: [
+    '@sanity/eslint-config-studio',
+    // This doesn't work for some reason
+    // require.resolve('config/eslint-preset'),
+  ],
+};
