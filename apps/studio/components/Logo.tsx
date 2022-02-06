7dfb54987055c4460f69f1841431c5f65c5069cb
Rikard Eide <rikardeide@gmail.com>
2022-02-06 18:25:54 +0100

    studio: customize Sanity with brand colors, logo and fonts

---
+++ apps/studio/components/Logo.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SanityLogo = () => {
+  return (
+    <img
+      src="/static/logo-feriekolonien-white.png"
+      alt="logo"
+      style={{
+        width: '160px',
+      }}
+    />
+  );
+};
+
+export default SanityLogo;
