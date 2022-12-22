4ac76b11c63cb3947835ab3c05ce6dc449eada2e
Rikard Eide <rikardeide@gmail.com>
2022-12-22 02:28:40 +0100

    web: add fallback text if no campdates are set
    If we remove/comment out the dates, we'll now show a "coming soon" text.

--- apps/web/siteConfig.ts
+++ apps/web/siteConfig.ts
@@ -3,9 +3,9 @@ const siteConfig = {
   waitingListEnabled: false,
   applicationURL: '/soknadsskjema',
   dates: [
-    { emoji: '🦀', name: '1. parti', dateRange: '21. juni – 1. juli' },
-    { emoji: '☀️', name: '2. parti', dateRange: '8. juli – 8. juli' },
-    { emoji: '🐚', name: '3. parti', dateRange: '25. juli – 4. august' },
+    // { emoji: '🦀', name: '1. parti', dateRange: '21. juni – 1. juli' },
+    // { emoji: '☀️', name: '2. parti', dateRange: '8. juli – 8. juli' },
+    // { emoji: '🐚', name: '3. parti', dateRange: '25. juli – 4. august' },
   ],
 };
 
--- apps/web/src/components/CampDates.tsx
+++ apps/web/src/components/CampDates.tsx
@@ -2,23 +2,27 @@ import siteConfig from '../../siteConfig';
 import Card from './Card';
 
 const CampDates = () => (
-  <div className="group relative mb-11 max-w-sm">
+  <div className="group relative mb-11 max-w-sm text-white">
     <Card className="absolute inset-0 mx-auto rotate-[-8deg] opacity-70 transition-all duration-300 ease-in-out group-hover:-rotate-3 md:ml-0" />
     <Card className="relative mx-auto shadow-xl md:ml-0">
-      <h2 className="mb-4 text-3xl font-bold text-white">Datoer for 2022</h2>
+      <h2 className="mb-4 text-3xl font-bold ">Datoer for 2023</h2>
 
-      <ul className="text-base text-white md:text-xl">
-        {siteConfig.dates.map((el) => (
-          <li key={el.name}>
-            <p className="flex">
-              <span className="mr-4 text-3xl">{el.emoji}</span>
-              <span className="font-semibold tracking-tight">
-                {el.name}
-              </span>: {el.dateRange}
-            </p>
-          </li>
-        ))}
-      </ul>
+      {siteConfig.dates.length ? (
+        <ul className="text-base  md:text-xl">
+          {siteConfig.dates.map((el) => (
+            <li key={el.name}>
+              <p className="flex">
+                <span className="mr-4 text-3xl">{el.emoji}</span>
+                <span className="font-semibold tracking-tight">
+                  {el.name}
+                </span>: {el.dateRange}
+              </p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className="">Kommer snart...</p>
+      )}
     </Card>
   </div>
 );
