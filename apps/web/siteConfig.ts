d217c086a4eea4b323fcdc42413cb4da7dbd2f69
Rikard Eide <rikardeide@gmail.com>
2024-12-23 00:18:25 +0100

    update release date

--- apps/web/siteConfig.ts
+++ apps/web/siteConfig.ts
@@ -1,26 +1,26 @@
 function isReleaseDate() {
-  return Date.now() > new Date("2025-12-26T00:00:00").getTime();
+  return Date.now() > new Date('2024-12-24T00:00:00').getTime();
 }
 
 const siteConfig = {
   openForApplication: () => isReleaseDate(),
   waitingListEnabled: false,
-  applicationURL: "/soknadsskjema",
+  applicationURL: '/soknadsskjema',
   getDates: () => [
     {
-      emoji: "🦀",
-      name: "1. parti",
-      dateRange: "24. juni - 4. juli",
+      emoji: '🦀',
+      name: '1. parti',
+      dateRange: '24. juni - 4. juli',
     },
     {
-      emoji: "☀️",
-      name: "2. parti",
-      dateRange: "11. juli - 21. juli",
+      emoji: '☀️',
+      name: '2. parti',
+      dateRange: '11. juli - 21. juli',
     },
     {
-      emoji: "🐚",
-      name: "3. parti",
-      dateRange: "28. juli - 7. august",
+      emoji: '🐚',
+      name: '3. parti',
+      dateRange: '28. juli - 7. august',
     },
   ],
 };
